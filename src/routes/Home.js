9a901ee935f4bfa70d8adb5f70e965aa092c9803
logan630 <logandp630@outlook.com>
2024-10-11 17:26:13 -0400

    style improvements to home page.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,15 +5,15 @@ import email_icon from '../assets/email-icon.svg';
 const Home = () => {
     return (
         <div className="homePage">
-            <div className="row">
+            {/* <div className="row"> */}
                 <div className="aboutMe">
                     <h1>Hi, I'm Logan.</h1>
                     <h2>I'm a New Jersey-based software developer.</h2>
-                    <p>I graduated from Stevens Institute of Technology in May 2024 with a <span className="emphasis">Bachelors of Science in Computer Science</span> with a concentration in <span className="emphasis">Machine Learning</span> and a minor in <span className="emphasis">Pure and Applied Mathematics</span>.</p>
+                    <p>I graduated from Stevens Institute of Technology in May 2024 with a <span className="emphasis">Bachelors of Science in Computer Science</span> with a concentration in <span className="emphasis">Machine Learning</span>, as well as a minor in <span className="emphasis">Pure and Applied Mathematics</span>.</p>
                     <p>I'm intersted in web development, and I'm especially interested in machine learning, theoretical computer science, mathematics, and the applications of mathematical theory to real-world problems in computer science. </p>
                 </div>
                 <div className="contact">
-                    <h1>My Links</h1>
+                    {/* <h1>My Links</h1> */}
                     <div className="icons">
                         <a href="https://github.com/logan630" target="_blank">
                             <img src={github_icon}></img>
@@ -23,7 +23,7 @@ const Home = () => {
                         </a>
                     </div>
                 </div>
-            </div>
+            {/* </div> */}
         </div>
     );
 }
